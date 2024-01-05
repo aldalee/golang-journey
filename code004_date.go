d29ba2b519bb2c5f4bd3571f9cc54969d04e5724
Huanyu <53891741+aldalee@users.noreply.github.com>
2024-01-05 16:23:32 +0800

    modify function

--- code004_date.go
+++ code004_date.go
@@ -1,7 +1,20 @@
 package main
 
-// 计算两个日期之间差多少天
+import "time"
 
+// 计算两个日期之间差多少天
+func DiffDays(t1, t2 time.Time) int {
+	if t1.IsZero() {
+		t1 = time.Now()
+	}
+	
+	t1Unix, _ := t1.Unix()
+	t2Unix, _ := t2.Unix()
+
+	diff := t2Unix - t1Unix
+
+	return int(diff / (24 * 3600))
+}
 // 推算几天后的日期
 
 
